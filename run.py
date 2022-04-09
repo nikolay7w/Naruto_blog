1d4695635e7f72cc720bade571ba1b4d4691efb9
nikolay7w <sadnikolasha@gmail.com>
2022-04-09 18:42:47 +0300

    'description'

--- run.py
+++ run.py
@@ -9,6 +9,30 @@ def home():
     return render_template('index.html', title='Главная')
 
 
+@app.route('/characters_page')
+def characters_page():
+    return render_template('characters_page.html')
+
+
+@app.route('/villages_page')
+def villages_page():
+    return render_template('villages_page.html')
+
+
+@app.route('/technology_page')
+def technology_page():
+    return render_template('technology_page.html')
+
+
+@app.route('/guns_page')
+def guns_page():
+    return render_template('guns_page.html')
+
+
+@app.route('/hronology_page')
+def hronology_page():
+    return render_template('hronology_page.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5656)
 
